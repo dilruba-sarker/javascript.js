baaafaa517e5d531d9359e43d7225f27e5f69b97
Dilruba-Sarker <dilrubasarkerseo@gmail.com>
2023-08-08 11:42:02 +0600

    next

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-function findingBadDate(arr){
-if(!Array.isArray===true){return 'please provide a valid array'}
-else{
-    let count=0;
-    
-    for(let i=0;i<arr.length;i++){
-        let element=arr[i];
-        if(typeof element !=="number"){
-            return "give me valid number"
-        }
-        else{
-            if(element<0){
-            count++;
-                
-                
-               
-            }
-        }
-    }
-
-    return  "total BadDate : " + count
+// -------------prime number----------
+function isPrime(n){
+if(typeof n !=="number" || n <=0){
+  return "provide valid input"
 }
-    
-}
+if(n<2){return `${n} is not a prime number`}
+for(let i=2;i<n;i++){
+if(n%i===0){return `${n} is not a prime number`}
 
-let array=[1,2,3,4,-3,-5,-7,-88,9,-1]
-    console.log( findingBadDate(array) )
+}
+{return `${n} is  a prime number`}
+}
+console.log( isPrime(17));
