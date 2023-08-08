d9d1fecd3d63b137cccb85557182f3d2c1af8f30
Dilruba-Sarker <dilrubasarkerseo@gmail.com>
2023-08-08 21:32:25 +0600

    next

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
-// number-------1
-function cubeNumber(number){
-    if(typeof number !=="number"){
-        return "please provide me valid number"
-    }else{
-        if(number>0){
-            return number **3
-        }
-        else{ return "Invalid input. Please provide a positive number."}
-    }
+// // number-------1
+// function cubeNumber(number){
+//     if(typeof number !=="number"){
+//         return "please provide me valid number"
+//     }else{
+//         if(number>0){
+//             return number **3
+//         }
+//         else{ return "Invalid input. Please provide a positive number."}
+//     }
+// }
+
+// console.log(cubeNumber("hjrher"));
+// console.log(cubeNumber(4));
+// console.log(cubeNumber(-2));
+
+
+
+
+// // ----------- problem 3
+
+
+
+
+// function sortMaker(arr) {
+//     if (arr[0] < 0 || arr[1] < 0) {
+//         return "Invalid Input";
+//     } else if (arr[0] === arr[1]) {
+//         return "equal";
+//     } else if ( arr[0]>0 && arr[1]>0) {
+//         return arr.sort((a, b) => b - a);
+//     } else {
+//         return arr;
+//     }
+// }
+
+// console.log(sortMaker([2, 3]));   
+// console.log(sortMaker([4, 2]));  
+// console.log(sortMaker([4, 4]));   
+// console.log(sortMaker([1, 2])); 
+// console.log(sortMaker([4, -2]));  
+
+// // ----------- problem 4
+
+function findAddress(obj) {
+
+    const street=obj.street || "__";
+    const house=obj. house || "__";
+    const society=obj.society || "__";
+    return `${street},${house},${society}`;
 }
 
-console.log(cubeNumber("hjrher"));
-console.log(cubeNumber(4));
-console.log(cubeNumber(-2));
+
+const obj={
+    
+  
+    society: "Earth Perfect"
+        
+}
+
+console.log(findAddress(obj));
