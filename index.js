b83f9a97d3d64ac4c1cc5dc42444b41b53db72f6
Dilruba-Sarker <dilrubasarkerseo@gmail.com>
2023-08-08 19:24:34 +0600

    next

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-// -------------prime number----------
-function isPrime(n){
-if(typeof n !=="number" || n <=0){
-  return "provide valid input"
+// number-------1
+function cubeNumber(number){
+    if(typeof number !=="number"){
+        return "please provide me valid number"
+    }else{
+        if(number>0){
+            return number **3
+        }
+        else{ return "Invalid input. Please provide a positive number."}
+    }
 }
-if(n<2){return `${n} is not a prime number`}
-for(let i=2;i<n;i++){
-if(n%i===0){return `${n} is not a prime number`}
 
-}
-{return `${n} is  a prime number`}
-}
-console.log( isPrime(17));
+console.log(cubeNumber("hjrher"));
+console.log(cubeNumber(4));
+console.log(cubeNumber(-2));
